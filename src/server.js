67bdc9a438a1c651cf9444b6bc8b041b9fe34679
Hernany <hernanytec777@Gmail.com>
2020-02-22 13:30:29 -0300

    adicionando cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const routes = require('./routes')
 
@@ -12,6 +13,7 @@ mongoose.connect('mongodb+srv://hernany:hernany@pid-lpswc.mongodb.net/pid?retryW
     useFindAndModify: false
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
